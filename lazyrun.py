5a8cd9a7de4038bb9df0ce0ec20e5eb6a316f830
Michael Gielda <mgielda@antmicro.com>
2021-07-07 19:01:32 +0200

    Contains actual code

--- lazyrun.py
+++ lazyrun.py
@@ -1,4 +1,26 @@
-# for now we will just have a few scripts in the apps directory
-# in the future these should be dynamically generated
-# then we parse yaml config, aggregate it with other configs potentially
-# and execute podman as needed 
+#!/usr/bin/env python3
+
+# default config should be included
+# user needs to copy it to config.yml
+# read additional "overlay" config, if specified as a parameter
+# perhaps later divide between "repositories" and masks (don't overcomplicate)
+# run through config objects and create appropriate symlinks
+
+import yaml
+
+config = None
+with open('config.yml', 'r') as f:
+    config = yaml.safe_load(f)
+
+for app in config:
+    print(f"Creating wrapper for {app}: {config[app]}")
+    path = 'apps/' + app
+    with open(path, 'w') as f:
+        f.write(f"podman run -it {config[app]} {app} $@")
+        import os
+        import stat
+
+        st = os.stat(path)
+        os.chmod(path, st.st_mode | stat.S_IEXEC)
+
+# later add way to include config in different file/path
